27e073f2c6952dd6937f63748093ac05f968738b
Pascal Yang <pascal@wustl.edu>
2021-02-02 18:56:00 +0800

    qwqqwq

---
+++ lll/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lll
+//
+//  Created by Pascal Yang on 2021/2/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
